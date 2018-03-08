31084ee3ced12ae3bd91fc9c4e360affffc50099
Vadim <vadimrus9911@gmail.com>
2018-03-08 08:00:14 +0500

    Update

--- zadanie13.cpp
+++ zadanie13.cpp
@@ -19,25 +19,55 @@ people power[lim];
 void save()
 {
  system("clear");
- FILE *f = NULL;
+ FILE *f;
  int i;
  cout << "Введите название файла: ";
  cin  >> namefile;
- f = fopen(namefile, "wb");
- for (i=0;i<n;i++)
+ f = fopen(namefile, "r");
+ if (!(f==NULL))
  {
-  fwrite(&power[n], sizeof(people), 1, f);
+  fclose(f);
+  cout << "Файл уже существует, перезаписать?\n";
+  cout << "Напишите 1, если хотите перезаписать,\nдругая клавиша просто сохранит содержимое массива в конец файла\n";
+  char p;
+  cin >> p;
+  if (p==1)
+  {
+   f = fopen(namefile, "wb");
+   for (i=0;i<n;i++)
+   {
+    fwrite(&power[i], sizeof(people), 1, f);
+   }
+   fclose(f);
+  }
+  else
+  {
+   f = fopen(namefile, "r+b");
+   fseek(f, 0, SEEK_END);
+   for (i=0;i<n;i++)
+   {
+    fwrite(&power[i], sizeof(people), 1, f);
+   }
+   fclose(f);
+  }
  }
- fclose(f);
+ else
+ {
+  f = fopen(namefile, "wb");
+  for (i=0;i<n;i++)
+  {
+   fwrite(&power[i], sizeof(people), 1, f);
+  }
+  fclose(f);
+ }
 }
 
-void odin(int n)
+void odin(int a)
 {
- n++;
- cout << "Магазин,  номер "    << n               << endl
-      << "Название магазина: " << power[n].store << endl
-      << "Название товара: "   << power[n].goods    << endl
-      << "Стоимость: "         << power[n].price << " руб." << endl;
+ cout << "Магазин,  номер "    << a               << endl
+      << "Название магазина: " << power[a].store  << endl
+      << "Название товара: "   << power[a].goods  << endl
+      << "Стоимость: "         << power[a].price  << " руб." << endl;
 }
 
 void find()
@@ -46,11 +76,9 @@ void find()
  char namestore[15];
  cout << "Введите название магазина: ";
  cin  >> namestore;
- n = 1;
- for (i = 0; i<lim; i++)
+ for (i = 0; i<n; i++)
  {
-  n++;
-  if (strcmp(namestore, power[n].store)==0)
+  if (strcmp(namestore, power[i].store)==0)
   {
    for (i=0;i<n;i++)
    odin(i);
@@ -70,14 +98,14 @@ void add()
  printf("Заполнено!\n");
  else
  {
-  n++;
-  cout << "Магазин,  номер "     << n          << endl
+  cout << "Магазин,  номер " << n << endl
        << "Введите название магазина: ";
   cin  >> power[n].store;
   cout << "Введите название товара: ";
   cin  >> power[n].goods;
   cout << "Введите цену: ";
   cin  >> power[n].price;
+  n++;
  }
 }
 
@@ -102,41 +130,39 @@ void loading()
  int z = ftell(f);
  cout << "Размер: " << z << endl;
  fseek(f, 0, SEEK_SET);
- int g = 1;
- n = n - 1;
+ cout << z/sizeof(people);
  for (i=0;i<z/sizeof(people);i++)
  {
   fread(&power[i], sizeof(people), 1, f);
-  cout << "Магазин, номер"      << g               << endl
-       << "Название магазина: " << power[n].store << endl
-       << "Название товара: "   << power[n].goods    << endl
-       << "Цена: "              << power[n].price     << endl;
-  n++;
-  g++;
+  cout << "Магазин, номер "     << i              << endl
+       << "Название магазина: " << power[i].store << endl
+       << "Название товара: "   << power[i].goods << endl
+       << "Цена: "              << power[i].price << endl;
  }
  fclose(f);
  system("pause");
 }
-/*
+
 void sort()
 {
- int counter = 0;
- n = 0;
- int j = 1;
- for (int i = 1; i < n; i++)
+ people h;
+ int k = 1, c = 3;
+ while(k==1)
  {
-  for ( j = i; j > 0 && power[n-1].store > power[n].store ;j++ )
+  k = 0;
+  for (int i = 0; i<c-1; i++)
   {
-   n++;
-   counter++;
-   char tmp[15] = power[n-1].store;
-   power[n-1].store = power[n].store;
-   power[n].store = tmp;
+   if (strcmp(power[i].store, power[i+1].store)>0)
+   {
+    h = power[i];
+    power[i] = power[i+1];
+    power[i+1] = h;
+    k = 1;
+   }
   }
  }
- cout << counter << endl;
 }
-*/
+
 
 int menu()
 {
@@ -147,7 +173,7 @@ int menu()
  printf("3 - поиск по названию магазина\n");
  printf("4 - добавить магазин\n");
  printf("5 - показать все магазины\n");
- //printf("6 - сортировка магазинов");
+ printf("6 - сортировка магазинов\n");
  printf("7 - выход\n");
  printf("Введите цифру: ");
  scanf("%d", &d);
@@ -170,7 +196,6 @@ int main()
           break;
    case 3:
           find();
-          //system("pause");
           break;
    case 4:
           add();
@@ -179,10 +204,9 @@ int main()
           all();
           system("pause");
           break;
-   /*case 6:
+   case 6:
           sort();
-          system("pause");
-          break;*/
+          break;
    case 7:
           return 0;
   }
